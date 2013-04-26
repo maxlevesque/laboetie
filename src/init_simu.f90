33103cd5eab88d8414644eadd07eee89bb4d3d70
Maximilien Levesque <maximilien.levesque@gmail.com>
2013-04-26 22:46:13 +0200

    Put subroutine in module.

--- src/init_simu.f90
+++ src/init_simu.f90
@@ -1,85 +1,58 @@
 subroutine init_simu
-  use precision_kinds
-  use mod_lbmodel, only: init_everything_related_to_lb_model => initialize
-  use io, only: print_header, print_input_in_output_folder
-  use myallocations
-  implicit none
+    use precision_kinds
+    use mod_lbmodel, only: init_everything_related_to_lb_model => initialize
+    use io, only: manageInputs => manage, print_header, print_input_in_output_folder, inquireNecessaryFilesExistence
+    use myallocations
+    implicit none
 
-  ! print header
-  call print_header
+    call print_header  ! print header
+    call inquireNecessaryFilesExistence  ! check that input, output folder and file ./lb.in exist
+    call manageInputs  ! go read the input file(s) and put everything in a character array
+    call init_everything_related_to_lb_model ! init everything related to D3Q15 or D3Q19 etc ie LB models
+    call supercell_definition    ! prepare supercell geometry
+    call scheduler ! t_equil, tmom, tmax! schedule simulation
+    call init_moments_for_LB    ! init moments for Lattice Boltzmann
+    call charges_init    ! init charge distribution
 
-  ! check that input, output folder and file ./lb.in exist
-block
-  logical :: file_exists
-  inquire(file="./output/.", exist=file_exists)
-  if( .not. file_exists) then
-    print*,"./output folder does not exist. I create one."
-    ! the system should be linux TODO TEST LINUX OR MAC DERIVATIVE HERE
-    call system('mkdir -p output') ! -p do not print error if exist and create parent directory if needed
-  end if
-  inquire(file="./lb.in", exist=file_exists)
-  if( .not. file_exists) stop "./lb.in does not exist"
-end block
+    contains
 
-  call put_input_in_character_array  ! go read the input file(s) and put everything in a character array
+        subroutine init_moments_for_LB
+            use precision_kinds, only: dp
+            use system, only: n, supercell
+            use input, only: input_dp
+            use mod_lbmodel, only: lbm
+            real(dp) :: initialSolventDensity
+            initialSolventDensity = input_dp('initialSolventDensity') ! read the initial, homogeneous, solvent density in input file
+            supercell%node%solventDensity = initialSolventDensity
+            if (.not. allocated(n)) call allocateReal4D(n)  ! zeroth order moment == population(r,v) == mass density
+        end subroutine init_moments_for_LB
 
-  call print_input_in_output_folder ! Print input parameters found to output folder
+        subroutine scheduler
+            use system, only: t_equil, tmom, tmax, D_iter, time
+            use input, only: input_int
+            ! 4 times are important :
+            ! - 0 at which simulation starts
+            ! - t_equil which is the time of equilibration ending
+            ! - tmom at which we're looking at tracer moment propagation
+            ! - tmax at which simulation stops
+            ! init to non-physical value catched later in order to be sure they are modified
+            time = 0
+            D_iter = input_int('D_iter')
+            tmax = input_int('tmax')
+            tmom = input_int('tmom')
+            t_equil = input_int('t_equil')
+            ! check coherence
+            if( tmax <= 0 .or. tmom <= 0 .or. t_equil <= 0 ) then
+                stop 'in scheduler. no time should be negative or zero'
+            end if
+            ! check tmax is last 
+            if( tmom > tmax .or. t_equil > tmax ) then
+                stop 'equilibration and moment propagation cannot start after simulation end. check input.'
+            end if
+            ! tracer moment preparation should come after equilibration
+            if( tmom < t_equil ) then
+                stop 'tracer moment propagation should come after equilibration.tmom should be >= t_equil. check input file.'
+            end if
+        end subroutine
 
-  call init_everything_related_to_lb_model ! init everything related to D3Q15 or D3Q19 etc ie LB models
-
-  ! prepare supercell geometry
-  call supercell_definition
-
-  ! schedule simulation
-  call scheduler ! t_equil, tmom, tmax
-
-  ! init moments for Lattice Boltzmann
-  call init_moments_for_LB
-
-  ! init charge distribution
-  call charges_init
-
-  contains
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-subroutine init_moments_for_LB
-  use precision_kinds, only: dp
-  use system, only: n, supercell
-  use input, only: input_dp
-  use mod_lbmodel, only: lbm
-  real(dp) :: initialSolventDensity
-  initialSolventDensity = input_dp('initialSolventDensity') ! read the initial, homogeneous, solvent density in input file
-  supercell%node%solventDensity = initialSolventDensity
-  if (.not. allocated(n)) call allocateReal4D(n)  ! zeroth order moment == population(r,v) == mass density
-end subroutine init_moments_for_LB
-
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-subroutine scheduler
-  use system, only: t_equil, tmom, tmax, D_iter, time
-  use input, only: input_int
-  ! 4 times are important :
-  ! - 0 at which simulation starts
-  ! - t_equil which is the time of equilibration ending
-  ! - tmom at which we're looking at tracer moment propagation
-  ! - tmax at which simulation stops
-  ! init to non-physical value catched later in order to be sure they are modified
-  time = 0
-  D_iter = input_int('D_iter')
-  tmax = input_int('tmax')
-  tmom = input_int('tmom')
-  t_equil = input_int('t_equil')
-  ! check coherence
-  if( tmax <= 0 .or. tmom <= 0 .or. t_equil <= 0 ) then
-    stop 'in scheduler. no time should be negative or zero'
-  end if
-  ! check tmax is last 
-  if( tmom > tmax .or. t_equil > tmax ) then
-    stop 'equilibration and moment propagation cannot start after simulation end. check input.'
-  end if
-  ! tracer moment preparation should come after equilibration
-  if( tmom < t_equil ) then
-    stop 'tracer moment propagation should come after equilibration.tmom should be >= t_equil. check input file.'
-  end if
-end subroutine
 end subroutine
--- src/module_io.f90
+++ src/module_io.f90
@@ -2,8 +2,14 @@ module io
     use precision_kinds
     use system, only: supercell
     use constants, only: x, y, z
+    use input , only : input_line
     implicit none
     contains
+    
+        subroutine manage
+            call put_input_in_character_array
+            call print_input_in_output_folder ! Print input parameters found to output folder
+        end subroutine
     
         ! this subroutine prints a file name 'filename' using x, y, z, array(x,y,z)
         subroutine print4darray(lx,ly,lz,array,filename )
@@ -171,5 +177,79 @@ module io
             end do
             close(10) ! close the .cube file called filename
             print*, filename,' written' ! warn user
-        end subroutine write_to_cube_file
-end module io
+        end subroutine
+        
+        subroutine inquireNecessaryFilesExistence
+            logical :: file_exists
+            inquire(file="./output/.", exist=file_exists)
+            if( .not. file_exists) then
+                call system('mkdir -p output') ! -p do not print error if exist and create parent directory if needed
+            end if
+            inquire(file="./lb.in", exist=file_exists)
+            if( .not. file_exists) stop "./lb.in, i.e. the input file, does not exist. STOP."
+        end subroutine
+        
+        subroutine put_input_in_character_array
+            integer(i2b) :: i, j, k, n ! dummy
+            integer(i2b) :: ios ! input output status of readen file
+            character(len=100) :: text ! temporary input line
+            character(len=100), allocatable, dimension(:) :: arraytemp  ! Temporary array to stock data for resizing input_line
+            character(len=len('lb.in')), parameter :: inputFilename = "lb.in"
+            integer(i2b) :: totalnumberofinputlines
+            ! open the file in which you have all inputs
+            open ( unit = 11 , file = inputFilename )
+            ! count the total number of lines (n) in input.in
+            ! a blank line counts for 0 if it is only a return to next line (\n)
+            ! a line with only blanks counts for 1
+            totalnumberofinputlines = 0
+            do while (.true.)
+                read ( 11 , '(a)' , iostat = ios ) text
+                if ( ios > 0 ) then
+                    write (*,*) 'error in input. critical'
+                    stop
+                else if ( ios < 0 ) then
+                    exit ! exit do loop and stop reading file
+                else
+                    if ( text /= '\n' ) totalnumberofinputlines = totalnumberofinputlines + 1
+                end if
+            end do
+            ! close file in order to open it properly from start
+            close ( unit = 11 )
+            ! allocate input_line so that it has the right number of lines
+            allocate ( input_line (totalnumberofinputlines) )
+            input_line = ' ' ! init
+            ! open again the input file
+            open ( unit = 11 , file = inputfilename )
+            !read it and put each line in input_line
+            do i = 1 , totalnumberofinputlines
+                read ( 11 , '(a)'  ) text
+                input_line (i) = trim ( adjustl (text) ) ! trim () removes trailing blanks while adjustl removes left blanks and put white blanks at the end
+            end do 
+            ! clean up comments in the lines (expl: option = 3 # blabla)
+            do i = 1 , totalnumberofinputlines
+                do j = 1 , len(text)
+                    if ( input_line (i) (j:j) == '#' ) then
+                        forall ( k = j : len(text) )
+                            input_line (i) (k:k) = ' '
+                        end forall
+                        exit
+                    end if
+                end do
+                input_line (i) = trim ( adjustl (input_line (i) ) )
+            end do
+            !Delete blank lines and count the size of the smallest array containing initial data  
+            n=0 !init
+            do i = 1 , totalnumberofinputlines
+                if ( input_line (i) (1:1) /= ' ' )  then
+                    input_line (n+1) = input_line(i)
+                    n = n + 1   
+                endif
+            end do
+            !Resize input_line to the smallest size by using a temporary array
+            arraytemp = input_line
+            deallocate ( input_line )
+            allocate ( input_line ( n ) )
+            input_line = arraytemp ( 1 : n  )
+        end subroutine put_input_in_character_array
+   
+end module
