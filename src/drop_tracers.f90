f66cabe9562da7437492f56b234bc3cc26c6674f
Maximilien Levesque <maximilien.levesque@gmail.com>
2014-02-10 14:01:25 +0100

    refrac

--- src/drop_tracers.f90
+++ src/drop_tracers.f90
@@ -5,49 +5,44 @@
 ! Imagine a very small droplet of radioactive particles, so few they do not change
 ! anything to the system, but numerous enough to be followed and make statistics.
 
-subroutine drop_tracers
+SUBROUTINE drop_tracers
 
-  use precision_kinds, only: dp, i2b
-  use system, only: tmom, tmax, elec_slope
-  use populations, only: calc_n_momprop
-  use moment_propagation, only: init, propagate, deallocate_propagated_quantity!, print_vacf, integrate_vacf!, not_yet_converged
-  use input, only: input_dp
+    USE precision_kinds, only: dp, i2b
+    USE system, only: tmom, tmax, elec_slope
+    USE populations, only: calc_n_momprop
+    USE moment_propagation, only: init, propagate, deallocate_propagated_quantity!, print_vacf, integrate_vacf!, not_yet_converged
+    USE input, only: input_dp
+    
+    IMPLICIT NONE
+    INTEGER(i2b) :: it
+    LOGICAL :: is_converged
 
-  implicit none
-  integer(kind=i2b) :: it
-  logical :: is_converged
+    PRINT*,'       step           VACF(x)                   VACF(y)                   VACF(z)'
+    PRINT*,'       ----------------------------------------------------------------------------------'
 
-  print*,'       step           VACF(x)                   VACF(y)                   VACF(z)'
-  print*,'       ----------------------------------------------------------------------------------'
+    CALL calc_n_momprop ! include elec_slope in population n
+    elec_slope = 0.0_dp ! turn the electric field off for the rest of mom_prop (included in n)
 
-  ! include elec_slope in population n
-  call calc_n_momprop
+    ! add electrostatic potential computed by the SOR routine an external contribution
+    ! elec_pot(singlx,ly,lz, ch, phi, phi2, t, t_equil);
+    ! call elec_pot
+    CALL init ! init moment propagation
 
-  ! turn the electric field off for the rest of mom_prop (included in n)
-  elec_slope = 0.0_dp
+    ! propagate in time
+    momproploop: DO it= 1, tmax-tmom
+    !  it=0
+    !  do while (not_yet_converged(it))
+    !   call elec_pot
+        call propagate (it,is_converged) ! propagate the propagated quantity
+    !    if( modulo(it,50000)==0 ) print_vacf
+    !    it = it + 1
+        IF( is_converged ) EXIT momproploop
+    END DO momproploop
 
-  ! add electrostatic potential computed by the SOR routine an external contribution
-  ! elec_pot(singlx,ly,lz, ch, phi, phi2, t, t_equil);
-  ! call elec_pot
+    PRINT*,
 
-  call init ! init moment propagation
+    !  CALL integrate_vacf ! compute the integral over time of vacf in each direction
+    !  CALL print_vacf ! print vacf to file vacf.dat
+    CALL deallocate_propagated_quantity
 
-
-  ! propagate in time
-  momproploop: do it= 1, tmax-tmom
-!  it=0
-!  do while (not_yet_converged(it))
-!   call elec_pot
-    call propagate (it,is_converged) ! propagate the propagated quantity
-!    if( modulo(it,50000)==0 ) print_vacf
-!    it = it + 1
-    if( is_converged ) exit momproploop
-  end do momproploop
-
-  print*,
-
-!  call integrate_vacf ! compute the integral over time of vacf in each direction
-!  call print_vacf ! print vacf to file vacf.dat
-  call deallocate_propagated_quantity
-
-end subroutine drop_tracers
+END SUBROUTINE drop_tracers
