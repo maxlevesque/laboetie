b0a80789f5fbb85f39cc9f8fc67262351c39e590
Maximilien Levesque <maximilien.levesque@gmail.com>
2014-02-12 18:28:45 +0100

    print location and quantity of adsorbed particles

--- src/mod_moment_propagation.f90
+++ src/mod_moment_propagation.f90
@@ -25,69 +25,82 @@ MODULE MOMENT_PROPAGATION
 ! ==============================================================================
 
 SUBROUTINE INIT
-  USE system, ONLY: phi, fluid, solid, n
-  USE input, ONLY: input_dp
-  REAL(dp) :: boltz_weight, Pstat, scattprop, scattprop_p, fermi, exp_dphi, exp_min_dphi
-  INTEGER(i2b) :: i, j, k, l, l_inv, ip, jp, kp
+    USE system, ONLY: phi, fluid, solid, n
+    USE input, ONLY: input_dp
+    REAL(dp) :: boltz_weight, Pstat, scattprop, scattprop_p, fermi, exp_dphi, exp_min_dphi
+    INTEGER(i2b) :: i, j, k, l, l_inv, ip, jp, kp
+    
+    tracer%ka = input_dp('tracer_ka')
+    tracer%kd = input_dp('tracer_kd')
+    IF( .NOT. testPositivity(tracer%ka) ) STOP 'I detected tracer%ka to be <0 in module moment_propagation. STOP.'
+    IF( .NOT. testPositivity(tracer%kd) ) STOP 'I detected tracer%kd to be <0 in module moment_propagation. STOP.'
+    IF(tracer%kd==0.0_dp) THEN
+        tracer%K = 0.0_dp
+    ELSE
+        tracer%K = (tracer%ka)/(tracer%kd)
+    END IF
+    tracer%z = input_dp('tracer_z')
+    tracer%Db = input_dp('tracer_Db') ! bulk diffusion coefficient of tracer, i.e. the molecular diffusion coefficient
+    tracer%Ds = input_dp('tracer_Ds') ! surface diffusion coefficient of tracer
+    IF (tracer%Db <= 0.0_dp ) STOP 'tracer_Db as readen in input is invalid'
+    IF (tracer%Ds /= 0.0_dp ) STOP "I've found a non-zero Ds (surface diffusion coefficient) in input file. Not implemented yet"
+    
+    lambda = calc_lambda()
+    lambda_s = calc_lambda_s()
+    
+    vacf = 0.0_dp
+    
+    lx = supercell%geometry%dimensions%indiceMax(x)
+    ly = supercell%geometry%dimensions%indiceMax(y)
+    lz = supercell%geometry%dimensions%indiceMax(z)
+    CALL test_and_allocate_what_is_needed_for_moment_propagation
+    
+    ! the sum of all boltzman weights is the sum over all exp(-z*phi) where supercell%node%nature == fluid. Note that is_interfacial == fluid + at interface
+    Pstat = sum(exp(-tracer%z*phi), mask=(supercell%node%nature==fluid))&
+            +sum(exp(-tracer%z*phi)*tracer%K, mask=supercell%node%nature==fluid .and. supercell%node%isInterfacial)
+    
+    DO concurrent (i=1:lx, j=1:ly, k=1:lz, supercell%node(i,j,k)%nature==fluid )
+        boltz_weight = exp(-tracer%z*phi(i,j,k))/Pstat ! boltz_weight=1/Pstat if tracer%z=0
 
-  tracer%ka = input_dp('tracer_ka')
-  tracer%kd = input_dp('tracer_kd')
-  IF( .NOT. testPositivity(tracer%ka) ) STOP 'I detected tracer%ka to be <0 in module moment_propagation. STOP.'
-  IF( .NOT. testPositivity(tracer%kd) ) STOP 'I detected tracer%kd to be <0 in module moment_propagation. STOP.'
-  IF(tracer%kd==0.0_dp) THEN
-    tracer%K = 0.0_dp
-  ELSE
-    tracer%K = (tracer%ka)/(tracer%kd)
-  END IF
-  tracer%z = input_dp('tracer_z')
-  tracer%Db = input_dp('tracer_Db') ! bulk diffusion coefficient of tracer, i.e. the molecular diffusion coefficient
-  tracer%Ds = input_dp('tracer_Ds') ! surface diffusion coefficient of tracer
-  IF (tracer%Db <= 0.0_dp ) STOP 'tracer_Db as readen in input is invalid'
-  IF (tracer%Ds /= 0.0_dp ) STOP "I've found a non-zero Ds (surface diffusion coefficient) in input file. Not implemented yet"
-
-  lambda = calc_lambda()
-  lambda_s = calc_lambda_s()
-
-  vacf = 0.0_dp
-
-
-  lx = supercell%geometry%dimensions%indiceMax(x)
-  ly = supercell%geometry%dimensions%indiceMax(y)
-  lz = supercell%geometry%dimensions%indiceMax(z)
-  CALL test_and_allocate_what_is_needed_for_moment_propagation
-
-  ! the sum of all boltzman weights is the sum over all exp(-z*phi) where supercell%node%nature == fluid. Note that is_interfacial == fluid + at interface
-  Pstat = sum(exp(-tracer%z*phi), mask=(supercell%node%nature==fluid))&
-         +sum(exp(-tracer%z*phi)*tracer%K, mask=supercell%node%nature==fluid .and. supercell%node%isInterfacial)
-
-  do concurrent (i=1:lx, j=1:ly, k=1:lz, supercell%node(i,j,k)%nature==fluid )
-    boltz_weight = exp(-tracer%z*phi(i,j,k))/Pstat ! boltz_weight=1/Pstat if tracer%z=0
-    do concurrent (l=lbm%lmin+1:lbm%lmax)
-      ip = pbc (i+lbm%vel(l)%coo(x) ,x)
-      jp = pbc (j+lbm%vel(l)%coo(y) ,y)
-      kp = pbc (k+lbm%vel(l)%coo(z) ,z)
-      if (supercell%node(ip,jp,kp)%nature==solid) cycle
-      exp_dphi = calc_exp_dphi( i, j, k, ip, jp, kp)
-      exp_min_dphi = 1.0_dp/exp_dphi ! =1 if tracer%z=0
-      fermi = 1.0_dp/(1.0_dp + exp_dphi) ! =0.5 if tracer%z=0
-      scattprop = calc_scattprop( n(i,j,k,l), supercell%node(i,j,k)%solventDensity, lbm%vel(l)%a0, lambda, fermi)
-      vacf(:,past) = vacf(:,past) + boltz_weight * scattprop * lbm%vel(l)%coo(:)**2
-      l_inv = lbm%vel(l)%inv
-      scattprop_p = calc_scattprop( &
-          n(ip,jp,kp,l_inv), supercell%node(ip,jp,kp)%solventDensity, lbm%vel(l_inv)%a0, lambda, 1.0_dp-fermi)
-      Propagated_Quantity(:,i,j,k,now) = Propagated_Quantity(:,i,j,k,now) &
-                 + exp_min_dphi * scattprop_p * lbm%vel(l_inv)%coo(:) * boltz_weight
+        DO concurrent (l=lbm%lmin+1:lbm%lmax)
+            ip = pbc (i+lbm%vel(l)%coo(x) ,x)
+            jp = pbc (j+lbm%vel(l)%coo(y) ,y)
+            kp = pbc (k+lbm%vel(l)%coo(z) ,z)
+            if (supercell%node(ip,jp,kp)%nature==solid) cycle
+            exp_dphi = calc_exp_dphi( i, j, k, ip, jp, kp)
+            exp_min_dphi = 1.0_dp/exp_dphi ! =1 if tracer%z=0
+            fermi = 1.0_dp/(1.0_dp + exp_dphi) ! =0.5 if tracer%z=0
+            scattprop = calc_scattprop( n(i,j,k,l), supercell%node(i,j,k)%solventDensity, lbm%vel(l)%a0, lambda, fermi)
+            vacf(:,past) = vacf(:,past) + boltz_weight * scattprop * lbm%vel(l)%coo(:)**2
+            l_inv = lbm%vel(l)%inv
+            scattprop_p = calc_scattprop( &
+                n(ip,jp,kp,l_inv), supercell%node(ip,jp,kp)%solventDensity, lbm%vel(l_inv)%a0, lambda, 1.0_dp-fermi)
+            Propagated_Quantity(:,i,j,k,now) = Propagated_Quantity(:,i,j,k,now) &
+                        + exp_min_dphi * scattprop_p * lbm%vel(l_inv)%coo(:) * boltz_weight
+        END DO
+        
+        IF(supercell%node(i,j,k)%isInterfacial .and. supercell%node(i,j,k)%nature==fluid) THEN
+            Propagated_Quantity_Adsorbed(:,i,j,k,now) = 0.0_dp
+        END IF
     end do
-    if(supercell%node(i,j,k)%isInterfacial .and. supercell%node(i,j,k)%nature==fluid) then
-        Propagated_Quantity_Adsorbed(:,i,j,k,now) = 0.0_dp
-    end if
-  end do
+    
+    PRINT*, 0, vacf(x,past), vacf(y,past), vacf(z,past)
+    
+    OPEN(99, file='output/vacf.dat')
+        WRITE(99,*)'# time t, VACF_x(t), VACF_y(t), VACF_z(t)'
+        WRITE(99,*) 0, vacf(x,past), vacf(y,past), vacf(z,past)
+    CLOSE(99)
 
-  print*, 0, vacf(x,past), vacf(y,past), vacf(z,past)
-  open(unit=99, file='output/vacf.dat')
-  write(99,*)'# time t, VACF_x(t), VACF_y(t), VACF_z(t)'
-  write(99,*) 0, vacf(x,past), vacf(y,past), vacf(z,past)
-  close(99)
+    OPEN(100, FILE='output/adsorbed_density.dat')
+        WRITE(100,*)
+        WRITE(100,*)"# time ",0
+        DO i=1,lx; DO j=1,ly; DO k=1,lz;
+            IF ( supercell%node(i,j,k)%isInterfacial .and. supercell%node(i,j,k)%nature==fluid ) THEN
+                WRITE(100,*)i,j,k,SUM(Propagated_Quantity_Adsorbed(:,i,j,k,now))
+            END IF
+        END DO; END DO; END DO;
+    CLOSE(100)
+
 END SUBROUTINE INIT
 
 ! ==============================================================================
@@ -113,54 +126,52 @@ SUBROUTINE PROPAGATE(it, is_converged)
         restpart = 1.0_dp ! fraction of particles staying at r; decreases in the loop over neighbours
     
         do l = lbm%lmin+1, lbm%lmax
-        ip = pbc(i+lbm%vel(l)%coo(x) ,x)
-        jp = pbc(j+lbm%vel(l)%coo(y) ,y)
-        kp = pbc(k+lbm%vel(l)%coo(z) ,z)
-        if ( supercell%node(ip,jp,kp)%nature /= fluid ) cycle
-        fermi = 1.0_dp/(1.0_dp + calc_exp_dphi(i,j,k,ip,jp,kp))
-        scattprop = calc_scattprop( n(i,j,k,l), supercell%node(i,j,k)%solventDensity, lbm%vel(l)%a0, lambda, fermi)
-        restpart = restpart - scattprop
-        scattprop_p = calc_scattprop( n(ip,jp,kp,lbm%vel(l)%inv), supercell%node(ip,jp,kp)%solventDensity,&
-                lbm%vel((lbm%vel(l)%inv))%a0, lambda, 1.0_dp-fermi)
-        Propagated_Quantity(:,i,j,k,next) = Propagated_Quantity(:,i,j,k,next) + Propagated_Quantity(:,ip,jp,kp,now)*scattprop_p
-        u_star = u_star + scattprop * lbm%vel(l)%coo(:)
+            ip = pbc(i+lbm%vel(l)%coo(x) ,x)
+            jp = pbc(j+lbm%vel(l)%coo(y) ,y)
+            kp = pbc(k+lbm%vel(l)%coo(z) ,z)
+            if ( supercell%node(ip,jp,kp)%nature /= fluid ) cycle
+            fermi = 1.0_dp/(1.0_dp + calc_exp_dphi(i,j,k,ip,jp,kp))
+            scattprop = calc_scattprop( n(i,j,k,l), supercell%node(i,j,k)%solventDensity, lbm%vel(l)%a0, lambda, fermi)
+            restpart = restpart - scattprop
+            scattprop_p = calc_scattprop( n(ip,jp,kp,lbm%vel(l)%inv), supercell%node(ip,jp,kp)%solventDensity,&
+                    lbm%vel((lbm%vel(l)%inv))%a0, lambda, 1.0_dp-fermi)
+            Propagated_Quantity(:,i,j,k,next) = Propagated_Quantity(:,i,j,k,next) + Propagated_Quantity(:,ip,jp,kp,now)*scattprop_p
+            u_star = u_star + scattprop * lbm%vel(l)%coo(:)
         end do
     
         vacf(:,now) = vacf(:,now) + Propagated_Quantity(:,i,j,k,now)*u_star(:)
     
         if (supercell%node(i,j,k)%isInterfacial .and. supercell%node(i,j,k)%nature==fluid) then
-        restpart = restpart - tracer%ka
-        if (restpart<0.0_dp) error=.true.
-    
-        Propagated_Quantity(:,i,j,k,next) = Propagated_Quantity (:,i,j,k,next) &
-            + restpart * Propagated_Quantity (:,i,j,k,now) &
-            + Propagated_Quantity_Adsorbed (:,i,j,k,now) * tracer%kd
-    
-        Propagated_Quantity_Adsorbed(:,i,j,k,next) = &
-            Propagated_Quantity_Adsorbed(:,i,j,k,now) * (1.0_dp - tracer%kd) &
-            + Propagated_Quantity(:,i,j,k,now)*tracer%ka
-    
-        vel: do concurrent (l=lbm%lmin+1:lbm%lmax)
-            ip = pbc(i+lbm%vel(l)%coo(x) ,x)
-            jp = pbc(j+lbm%vel(l)%coo(y) ,y)
-            kp = pbc(k+lbm%vel(l)%coo(z) ,z)
-            if (.not. (supercell%node(ip,jp,kp)%isInterfacial .and. supercell%node(ip,jp,kp)%nature==fluid)) cycle ! is_interfacial is fluid AND interface
-            fermi = 1.0_dp/(1.0_dp + calc_exp_dphi(i,j,k,ip,jp,kp)) ! 1/2 when tracer has no charge
-            scattprop = calc_scattprop( n(i,j,k,l), supercell%node(i,j,k)%solventDensity, lbm%vel(l)%a0, lambda_s, fermi)
-            restpart = restpart - scattprop
-            l_inv = lbm%vel(l)%inv
-            scattprop_p = calc_scattprop( n(ip,jp,kp,l_inv), supercell%node(ip,jp,kp)%solventDensity, lbm%vel(l_inv)%a0,&
-                                                                lambda_s, 1.0_dp-fermi)
-            Propagated_Quantity_adsorbed (:,i,j,k,next) = &
-                Propagated_Quantity_adsorbed (:,i,j,k,next) &
-            + Propagated_Quantity_adsorbed (:,ip,jp,kp,now)*scattprop_p
-            u_star = u_star + scattprop* lbm%vel(l)%coo(:)
-        end do vel
-    
-    
-    
+            restpart = restpart - tracer%ka
+            if (restpart<0.0_dp) error=.true.
+        
+            Propagated_Quantity(:,i,j,k,next) = Propagated_Quantity (:,i,j,k,next) &
+                + restpart * Propagated_Quantity (:,i,j,k,now) &
+                + Propagated_Quantity_Adsorbed (:,i,j,k,now) * tracer%kd
+        
+            Propagated_Quantity_Adsorbed(:,i,j,k,next) = &
+                Propagated_Quantity_Adsorbed(:,i,j,k,now) * (1.0_dp - tracer%kd) &
+                + Propagated_Quantity(:,i,j,k,now)*tracer%ka
+        
+            do concurrent (l=lbm%lmin+1:lbm%lmax)
+                ip = pbc(i+lbm%vel(l)%coo(x) ,x)
+                jp = pbc(j+lbm%vel(l)%coo(y) ,y)
+                kp = pbc(k+lbm%vel(l)%coo(z) ,z)
+                if (.not. (supercell%node(ip,jp,kp)%isInterfacial .and. supercell%node(ip,jp,kp)%nature==fluid)) cycle ! is_interfacial is fluid AND interface
+                fermi = 1.0_dp/(1.0_dp + calc_exp_dphi(i,j,k,ip,jp,kp)) ! 1/2 when tracer has no charge
+                scattprop = calc_scattprop( n(i,j,k,l), supercell%node(i,j,k)%solventDensity, lbm%vel(l)%a0, lambda_s, fermi)
+                restpart = restpart - scattprop
+                l_inv = lbm%vel(l)%inv
+                scattprop_p = calc_scattprop( n(ip,jp,kp,l_inv), supercell%node(ip,jp,kp)%solventDensity, lbm%vel(l_inv)%a0,&
+                                                                    lambda_s, 1.0_dp-fermi)
+                Propagated_Quantity_adsorbed (:,i,j,k,next) = &
+                    Propagated_Quantity_adsorbed (:,i,j,k,next) &
+                + Propagated_Quantity_adsorbed (:,ip,jp,kp,now)*scattprop_p
+                u_star = u_star + scattprop* lbm%vel(l)%coo(:)
+            end do
+        
         else if( .not. (supercell%node(i,j,k)%isInterfacial .and. supercell%node(i,j,k)%nature==fluid)) then
-        Propagated_Quantity(:,i,j,k,next) = Propagated_Quantity(:,i,j,k,next) + restpart*Propagated_Quantity(:,i,j,k,now)
+            Propagated_Quantity(:,i,j,k,next) = Propagated_Quantity(:,i,j,k,next) + restpart*Propagated_Quantity(:,i,j,k,now)
         end if
     
     end do fluidnodes
@@ -174,9 +185,19 @@ SUBROUTINE PROPAGATE(it, is_converged)
     Propagated_Quantity(:,:,:,:,next) = 0.0_dp
     Propagated_Quantity_Adsorbed(:,:,:,:,now) = Propagated_Quantity_Adsorbed(:,:,:,:,next)
     Propagated_Quantity_Adsorbed(:,:,:,:,next) = 0.0_dp
-    
+
     OPEN(99, FILE='output/vacf.dat', ACCESS='append')
         WRITE(99,*) it, vacf(x,now), vacf(y,now), vacf(z,now)
+    
+    OPEN(100, FILE='output/adsorbed_density.dat', ACCESS='append')
+        WRITE(100,*)
+        WRITE(100,*)"# time ",it
+        DO i=1,lx; DO j=1,ly; DO k=1,lz;
+            IF ( supercell%node(i,j,k)%isInterfacial .and. supercell%node(i,j,k)%nature==fluid ) THEN
+                WRITE(100,*)i,j,k,SUM(Propagated_Quantity_Adsorbed(:,i,j,k,now))
+            END IF
+        END DO; END DO; END DO;
+    CLOSE(100)
     
     vacf(:,past) = vacf(:,now)
     vacf(:,now) = 0.0_dp
