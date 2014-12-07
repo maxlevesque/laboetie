75e2125055a4679d96f4bc71aa5e863a309c68ca
Maximilien Levesque <maximilien.levesque@gmail.com>
2014-12-07 18:40:12 +0100

    :lipstick: loop unname

--- src/equilibration_without_constraint.f90
+++ src/equilibration_without_constraint.f90
@@ -20,21 +20,18 @@ subroutine equilibration_without_constraint
   elec_slope = 0.0_dp
   lncb_slope = 0.0_dp
 
-  timeloop: do time = 1, t_equil
-
-  call update_populations      ! get populations
-  call propagation ! propagation of the distribution functions according to their direction to the next nodes
-  call comp_rho    ! compute density
-  call comp_j      ! compute momenta
-  call advect      ! solute motion: advection step
-
-  do iteration= 1, d_iter   ! solute motion: diffusion step
-    call sor                ! compute phi with sucessive overrelaxation method
-    call electrostatic_pot  ! compute phi + phi_external (due to electrostatic field elec_slope(x:z))
-    call smolu              ! this time full smolu, not just_equ_smolu
-    call charge_test        ! check charge conservation
+  do time = 1, t_equil
+    call update_populations      ! get populations
+    call propagation ! propagation of the distribution functions according to their direction to the next nodes
+    call comp_rho    ! compute density
+    call comp_j      ! compute momenta
+    call advect      ! solute motion: advection step
+    do iteration= 1, d_iter   ! solute motion: diffusion step
+      call sor                ! compute phi with sucessive overrelaxation method
+      call electrostatic_pot  ! compute phi + phi_external (due to electrostatic field elec_slope(x:z))
+      call smolu              ! this time full smolu, not just_equ_smolu
+      call charge_test        ! check charge conservation
+    end do
   end do
-
-  end do timeloop
 
 end subroutine equilibration_without_constraint
