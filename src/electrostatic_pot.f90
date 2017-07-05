feaa6724b09b51bb21ba138b548fd524f95de1ce
Maximilien Levesque <maximilien.levesque@gmail.com>
2017-07-05 19:16:10 +0200

    Refactor a little

--- src/electrostatic_pot.f90
+++ src/electrostatic_pot.f90
@@ -1,44 +1,40 @@
-! Here we compute the total electrostatic potential which is the sum
-! of the internal potential (phi) computed in routine sor by the
-! successive over relaxation method, and of the external electrostatic
-! potential.
+! Here we compute the total electrostatic potential.
+! It is the sum of the internal potential, phi, that should have been computed before for instance by Successive Over Relaxation,
+! and of an some external electric potential.
 
 subroutine electrostatic_pot
-    use precision_kinds, only: i2b, dp
+
+    use precision_kinds, only: dp
     use system, only: elec_slope, phi, phi_tot, supercell
-    use constants, only: x, y, z, zerodp
-    USE myallocations
+
     implicit none
-    integer(kind=i2b) :: i, j, k, lz, m, half, ly, lx ! dummy
-    ! ----------- Ade : 21/02/17 ------------------------------
-    if(.not.allocated(phi_tot)) call allocateReal3D(phi_tot)
-    phi_tot(:,:,:) = zerodp 
-    !open(375, file = "output/phiELECPOT.dat")
-    lx = supercell%geometry%dimensions%indiceMax(x)
-    ly = supercell%geometry%dimensions%indiceMax(y)
-    lz = supercell%geometry%dimensions%indiceMax(z)
-    half = lz/2
-    ! ----------- Ade : 21/02/17 ------------------------------
 
-    ! if no external electrostatics, don't loop: phi_tot = phi calculated by SOR
+    integer :: i, j, k, lx, ly, lz
+    real(dp) :: potx, poty, potz
+    real(dp), parameter :: zerodp = 0._dp
 
-    if( all(elec_slope==0.0_dp) ) then
+    lx = supercell%geometry%dimensions%indiceMax(1)
+    ly = supercell%geometry%dimensions%indiceMax(2)
+    lz = supercell%geometry%dimensions%indiceMax(3)
+
+    if( .not. allocated(phi_tot)) allocate( phi_tot(lx,ly,lz), source=zerodp)
+
+    ! If no external electrostatics, don't loop: phi_tot = phi calculated by SOR
+    ! else compute phi_tot as the sum of phi calculated by SOR and external contribution
+
+    if( all( elec_slope == 0.0_dp ) ) then
         phi_tot = phi
     else
-    !DO m=1,lz
-       !WRITE(375,*) 'Elec_slope = ', elec_slope(1), elec_slope(2), elec_slope(3)
-       !WRITE(375,*) m, SUM(phi(:,:,m))
-    !ENDDO
-        ! else compute phi_tot as the sum of phi calculated by SOR and external contribution
-        do concurrent( k= 1:supercell%geometry%dimensions%indiceMax(z),&
-                       j= 1:supercell%geometry%dimensions%indiceMax(y),&
-                       i= 1:supercell%geometry%dimensions%indiceMax(x) )
-            phi_tot(i,j,k) = phi(i,j,k) + ( elec_slope(1) * real(i-1,dp) + elec_slope(2)*real(j-1,dp) + elec_slope(3)*real(k-1,dp) )
+        do k = 1, lz
+            potz = real(k-1,dp) * elec_slope(3)
+            do j = 1, ly
+                poty = real(j-1,dp) * elec_slope(2)
+                do i = 1, lx
+                    potx = real(i-1,dp) * elec_slope(1)
+                    phi_tot(i,j,k) = phi(i,j,k) + potx + poty + potz
+                end do
+            end do
         end do
-        !write(375,*) '# half = ', half
-        !DO m=1,ly
-        !    WRITE(375,*) m, (phi_tot(:,m,half))
-        !ENDDO
     end if
-    !close(375)
+
 end subroutine electrostatic_pot
