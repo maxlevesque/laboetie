d9a0bc37f37c9ec8e2ed1c360f49084fadfe7c9d
Maximilien Levesque <maximilien.levesque@gmail.com>
2017-07-05 16:44:35 +0200

    Refactor advect.f90 :lipstick: and remove useless stuff

--- src/advect.f90
+++ src/advect.f90
@@ -1,38 +1,33 @@
 ! Advect charge density using the volumetric interpretation of the lattice boltzmann
 subroutine advect
-  use precision_kinds, only: dp, i2b
-  use constants, only: x, y, z, zerodp
+  
+  use precision_kinds, only: dp
+  use constants, only: x, y, z
   use system, only: c_plus, c_minus, flux_site_minus, flux_site_plus, el_curr_x, el_curr_y, el_curr_z, &
-  ion_curr_x, ion_curr_y, ion_curr_z, fluid, pbc, supercell, node
-  use myallocations
+                    ion_curr_x, ion_curr_y, ion_curr_z, fluid, pbc, supercell, node
+  use myallocations, only: allocateReal3D
+  
   implicit none
-  real(dp) :: c_minus_total_old, c_plus_total_old, c_minus_total_new, c_plus_total_new
-  integer(i2b) :: i, j, k, ix, iy, iz, ip, jp, kp, ip_all(-1:1), jp_all(-1:1), kp_all(-1:1)
+  
+  integer :: i, j, k, ix, iy, iz, ip, jp, kp, ip_all(-1:1), jp_all(-1:1), kp_all(-1:1)
   real(dp) :: vx, vy, vz, ax, ay, az
   real(dp) :: flux_link_minus, flux_link_plus
-  !call allocateReal3D( flux_site_minus )
-  !call allocateReal3D( flux_site_plus )
+  integer :: natureNodeijk, natureNodeipjpkp ! dummy variables
+  real(dp) :: c_plus_ijk, c_minus_ijk
+  real(dp), parameter :: zerodp = 0._dp, epsdp = epsilon(1._dp)
+
   if (.not. allocated(flux_site_plus)) call allocateReal3D(flux_site_plus)
   if (.not. allocated(flux_site_minus)) call allocateReal3D(flux_site_minus)
   
   flux_site_plus = zerodp
   flux_site_minus = zerodp
 
-  ! Ade : This is a temporary test. To be removed
-  !-------------------- Ade --------------------------- 
-  ! init ion (solute) concentrations
-  if (.not. allocated(c_plus)) call allocateReal3D(c_plus)
+  if (.not. allocated(c_plus))  call allocateReal3D( c_plus)
   if (.not. allocated(c_minus)) call allocateReal3D( c_minus)
-  !-------------------- Ade --------------------------- 
 
-  ! Ade: for Debug
-  open(415, file = "output/IonCurrX.dat")
-  open(416, file = "output/IonCurrY.dat")
-  open(417, file = "output/IonCurrZ.dat")
-  ! end debug
 
-  !   THIS IS VERY CRIPTIC...
-  !   What I am doing is what discussed with you.
+  !  THIS IS VERY CRIPTIC...
+  !  What I am doing is what discussed with you.
   !  Basically (when using this word it means that the explanation is a little bit mistic :-) )
   !  you have to compute the portion of the volume which goes in one of the neighbouring 26 cells.
   !  The part of the volume that goes in the next cell is ax*ay*az.
@@ -46,142 +41,110 @@ subroutine advect
   !  Forget this explanation. It works!!!!
 
   ! convective contribution to the electric current
-  if ( all(abs(c_plus)<=epsilon(1._dp)) .and. all(abs(c_minus)<=epsilon(1._dp)) ) then ! advection of charge is useless since there is no charge!
-    return
+  if ( all(abs(c_plus)<=epsdp) .and. all(abs(c_minus)<=epsdp) ) then ! advection of charge is useless since there is no charge!
+      return
   end if
 
-  if (all(abs(c_plus-c_minus)<=epsilon(1._dp))) then
-    el_curr_x = 0._dp
-    el_curr_y = 0._dp
-    el_curr_z = 0._dp
-  else ! Ade : the triple do loop is needed because of /node(i,j,k)%solventDensity
-      el_curr_x  = -sum( node%solventFlux(x)/node%solventDensity*( c_plus - c_minus ) , mask=node%nature==fluid) ! Ade : do node%solventFlux(x)/node%density. DONE
-      el_curr_y  = -sum( node%solventFlux(y)/node%solventDensity*( c_plus - c_minus ) , mask=node%nature==fluid)
-      el_curr_z  = -sum( node%solventFlux(z)/node%solventDensity*( c_plus - c_minus ) , mask=node%nature==fluid)
-  end if
-
-  if (all(abs(c_plus+c_minus)<=epsilon(1._dp))) then
-    ion_curr_x = 0._dp
-    ion_curr_y = 0._dp
-    ion_curr_z = 0._dp
+  if (all(abs(c_plus-c_minus)<=epsdp)) then
+      el_curr_x = 0._dp
+      el_curr_y = 0._dp
+      el_curr_z = 0._dp
   else
-      ion_curr_x  = -sum( node%solventFlux(x)/node%solventDensity*( c_plus + c_minus ) , mask=node%nature==fluid)
-      ion_curr_y  = -sum( node%solventFlux(y)/node%solventDensity*( c_plus + c_minus ) , mask=node%nature==fluid)
-      ion_curr_z  = -sum( node%solventFlux(z)/node%solventDensity*( c_plus + c_minus ) , mask=node%nature==fluid)
+      el_curr_x  = -sum( node%solventFlux(1)/node%solventDensity*( c_plus - c_minus ) , mask=node%nature==fluid)
+      el_curr_y  = -sum( node%solventFlux(2)/node%solventDensity*( c_plus - c_minus ) , mask=node%nature==fluid)
+      el_curr_z  = -sum( node%solventFlux(3)/node%solventDensity*( c_plus - c_minus ) , mask=node%nature==fluid)
+  end if
+
+  if (all(abs(c_plus+c_minus)<=epsdp)) then
+      ion_curr_x = 0._dp
+      ion_curr_y = 0._dp
+      ion_curr_z = 0._dp
+  else
+      ion_curr_x  = -sum( node%solventFlux(1)/node%solventDensity*( c_plus + c_minus ) , mask=node%nature==fluid)
+      ion_curr_y  = -sum( node%solventFlux(2)/node%solventDensity*( c_plus + c_minus ) , mask=node%nature==fluid)
+      ion_curr_z  = -sum( node%solventFlux(3)/node%solventDensity*( c_plus + c_minus ) , mask=node%nature==fluid)
   end if
 
 
-  ! Ade : Debug 
-  write(415,*) '##############################################'
-  write(416,*) '##############################################'
-  write(417,*) '##############################################'
-  write(415,*) ion_curr_x, el_curr_x
-  write(416,*) ion_curr_y, el_curr_y
-  write(417,*) ion_curr_z, el_curr_z
+  ! ! compute concentration before advection step
+  ! c_plus_total_old  = sum( c_plus)
+  ! c_minus_total_old = sum( c_minus)
 
-  !End Debug
-
-  ! compute concentration before advection step
-  c_plus_total_old = sum( c_plus)
-  c_minus_total_old = sum( c_minus)
-
-
-
-  !-------------------- Ade --------------------------- 
-  ip_all = [0,0,0]
-  jp_all = [0,0,0]
-  kp_all = [0,0,0]
-
-  ix = 0
-  iy = 0
-  iz = 0
-
-  ip = 0
-  jp = 0
-  kp = 0
-  !-------------------- Ade --------------------------- 
-  
   do i= supercell%geometry%dimensions%indiceMin(x), supercell%geometry%dimensions%indiceMax(x)
-    
-    !do ix = -1,1
-    !    ip_all(ix) = pbc( i+ix ,x)
-    !    print*, 'ix dans la boucle vaut ', ix
-    !end do     
     ip_all(:) = [(pbc(i+ix,x),ix=-1,1)]
     
     do j= supercell%geometry%dimensions%indiceMin(y), supercell%geometry%dimensions%indiceMax(y)
-      
-      !do iy = -1,1
-      !    jp_all(iy) = pbc( j+iy ,y)
-      !    print*, 'iy dans la boucle vaut ', iy
-      !end do     
       jp_all(:) = [(pbc(j+iy,y),iy=-1,1)]
       
       do k= supercell%geometry%dimensions%indiceMin(z), supercell%geometry%dimensions%indiceMax(z)
+        kp_all(:) = [(pbc(k+iz,z),iz=-1,1)]
+
         if( abs(c_plus(i,j,k))<=epsilon(1._dp) .and. abs(c_minus(i,j,k))<=epsilon(1._dp) ) cycle
         
-        !do iz = -1,1
-        !    kp_all(iz) = pbc( k+iz ,z)
-        !    print*, 'iz dans la boucle vaut ', iz
-        !end do     
-        kp_all(:) = [(pbc(k+iz,z),iz=-1,1)]
-        
+        natureNodeijk = node(i,j,k)%nature
+        c_plus_ijk = c_plus(i,j,k)
+        c_minus_ijk = c_minus(i,j,k)
+
         ! velocities
-        if(node(i,j,k)%nature==fluid) then ! Ade : the if structure is to avoid division by zero since solventDensity = 0 in solid nodes
-            vx = node(i,j,k)%solventFlux(x)/node(i,j,k)%solventDensity ! Ade : divide again by node%density. DONE
-            vy = node(i,j,k)%solventFlux(y)/node(i,j,k)%solventDensity
-            vz = node(i,j,k)%solventFlux(z)/node(i,j,k)%solventDensity
+        if( natureNodeijk == fluid) then ! Avoid division by zero where solventDensity is 0, i.e. in solid nodes
+            vx = node(i,j,k)%solventFlux(1) / node(i,j,k)%solventDensity
+            vy = node(i,j,k)%solventFlux(2) / node(i,j,k)%solventDensity
+            vz = node(i,j,k)%solventFlux(3) / node(i,j,k)%solventDensity
         else
-            vx = node(i,j,k)%solventFlux(x)
-            vy = node(i,j,k)%solventFlux(y)
-            vz = node(i,j,k)%solventFlux(z)
-        endif 
+            vx = node(i,j,k)%solventFlux(1)
+            vy = node(i,j,k)%solventFlux(2)
+            vz = node(i,j,k)%solventFlux(3)
+        endif
+ 
         do ix= -1, 1 ! TODO improve here by getting things out of useless loops
-          ax = vx * real(ix,dp)
-          do iy= -1, 1
-            ay = vy * real(iy,dp)
-            do iz= -1, 1
-              ip = ip_all(ix) 
-              jp = jp_all(iy)
-              kp = kp_all(iz)
-              az = vz * real(iz,dp)
-              ! check if link is accessible
-              if (all([ax,ay,az]>=0._dp) .and. node(i,j,k)%nature==fluid .and. node(ip,jp,kp)%nature==fluid) then ! link is accessible
-                if( ix == 0 ) ax = 1.0_dp - abs(vx)
-                if( iy == 0 ) ay = 1.0_dp - abs(vy)
-                if( iz == 0 ) az = 1.0_dp - abs(vz)
-                flux_link_plus  = ax*ay*az* c_plus (i,j,k)
-                flux_link_minus = ax*ay*az* c_minus(i,j,k)
-              else ! link not accessible => no flux
-                flux_link_plus  = 0.0_dp
-                flux_link_minus = 0.0_dp
-              end if
-              ! remove what is leaving site i,j,k and add what is arriving at ip,jp,kp
-              flux_site_minus(i,j,k)    = flux_site_minus(i,j,k)    - flux_link_minus
-              flux_site_minus(ip,jp,kp) = flux_site_minus(ip,jp,kp) + flux_link_minus
-              flux_site_plus (i,j,k)    = flux_site_plus (i,j,k)    - flux_link_plus
-              flux_site_plus (ip,jp,kp) = flux_site_plus (ip,jp,kp) + flux_link_plus
+            ax = vx * real(ix,dp)
+            ip = ip_all(ix) 
+
+            do iy= -1, 1
+                ay = vy * real(iy,dp)
+                jp = jp_all(iy)
+            
+                do iz= -1, 1
+                    az = vz * real(iz,dp)
+                    kp = kp_all(iz)
+
+                    natureNodeipjpkp = node(ip,jp,kp)%nature
+                    
+                    ! check if link is accessible
+                    if ( ax>=0._dp .and. ay>=0._dp .and. az>=0._dp .and. natureNodeijk==fluid .and. natureNodeipjpkp==fluid) then ! link is accessible
+                        if( ix == 0 ) ax = 1.0_dp - abs(vx)
+                        if( iy == 0 ) ay = 1.0_dp - abs(vy)
+                        if( iz == 0 ) az = 1.0_dp - abs(vz)
+                        flux_link_plus  = ax*ay*az* c_plus_ijk
+                        flux_link_minus = ax*ay*az* c_minus_ijk
+                    else ! link not accessible => no flux
+                        flux_link_plus  = 0.0_dp
+                        flux_link_minus = 0.0_dp
+                    end if
+
+                    ! remove what is leaving site i,j,k and add what is arriving at ip,jp,kp
+                    flux_site_minus(i,j,k)    = flux_site_minus(i,j,k)    - flux_link_minus
+                    flux_site_minus(ip,jp,kp) = flux_site_minus(ip,jp,kp) + flux_link_minus
+                    flux_site_plus (i,j,k)    = flux_site_plus (i,j,k)    - flux_link_plus
+                    flux_site_plus (ip,jp,kp) = flux_site_plus (ip,jp,kp) + flux_link_plus
+            
+                end do
             end do
-          end do
         end do
+      
       end do
     end do
   end do
   
 
   ! update concentrations accordingly to flux calculated previously
-  where( node%nature == fluid )
-    c_minus = c_minus + flux_site_minus
-    c_plus  = c_plus  + flux_site_plus
-  end where
+  c_minus = c_minus + flux_site_minus
+  c_plus  = c_plus  + flux_site_plus
 
   ! compute concentrations after advection
-  c_minus_total_new = sum(c_minus)
-  c_plus_total_new = sum(c_plus)
+  ! c_minus_total_new = sum(c_minus)
+  ! c_plus_total_new = sum(c_plus)
 
-  close(415)
-  close(416)
-  close(417)
 
   ! check that total concentration has not changed
   !if( abs( c_minus_total_new - c_minus_total_old ) > 1.e-8 .or. abs( c_plus_total_new - c_plus_total_old ) > 1.e-8 ) then ! TODO magic number
